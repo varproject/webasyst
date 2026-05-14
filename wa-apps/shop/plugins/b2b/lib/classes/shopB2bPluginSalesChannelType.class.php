1c86b9da6274a7174148df64e843350aeae61c61
Vagram <tigra_pv@mail.ru>
2026-05-15 01:37:38 +0300

    b2b

--- wa-apps/shop/plugins/b2b/lib/classes/shopB2bPluginSalesChannelType.class.php
+++ wa-apps/shop/plugins/b2b/lib/classes/shopB2bPluginSalesChannelType.class.php
@@ -2,82 +2,13 @@
 
 class shopB2bPluginSalesChannelType extends shopSalesChannelType
 {
-    // Штатные поля
+    // Штатные поля.
     protected function getBaseFieldsConfig(): array
     {
         $res = parent::getBaseFieldsConfig();
-        // $res['name']['class'] = 'width-50';
         $res['description']['class'] = 'smallest';
+
         return $res;
-    }
-
-    // Пля формы настройки канала продаж
-    protected function getFormFieldsConfig($values = []): array
-    {
-        $frontend_from_root = !empty($values['frontend_from_root'])
-            || ifset($values, 'frontend_url', '') === '*';
-
-        $frontend_url_value = ifset($values, 'frontend_url', 'b2b');
-
-        if ($frontend_url_value === '*') {
-            $frontend_url_value = 'b2b';
-        }
-
-        return [
-            'route_key' => [
-                'title' => 'Поселение',
-                'description' => 'Выберите домен, через которое будет открываться клиентская B2B-витрина.',
-                'control_type' => waHtmlControl::SELECT,
-                // 'class' => 'width-50',
-                'options' => $this->getShopRouteOptions(),
-                'value' => ifset($values, 'route_key', ''),
-            ],
-
-            // 'frontend_url' => [
-            //     'title' => 'Адрес витрины',
-            //     'description' => 'Укажите корневой URL витрины. Например: b2b, clients, portal.',
-            //     'control_type' => waHtmlControl::INPUT,
-            //     // 'class' => 'width-50',
-            //     'value' => $frontend_url_value,
-            //     'disabled' => $frontend_from_root,
-            // ],
-
-            'frontend_from_root' => [
-                'title' => 'Сделать от корня',
-                'description' => 'B2B-витрина будет открываться от корня выбранного поселения.' . $this->getFrontendFromRootScript(),
-                'control_type' => waHtmlControl::CHECKBOX,
-                'class' => 'checkbox',
-                'value' => $frontend_from_root ? 1 : 0,
-            ],
-
-            'frontend_url' => [
-                'title' => 'Адрес витрины',
-                'description' => 'Укажите корневой URL витрины. Например: b2b, clients, portal.',
-                'control_type' => waHtmlControl::INPUT,
-                'class' => 'small',
-                'value' => $frontend_url_value,
-                'disabled' => $frontend_from_root,
-                'control_wrapper' =>
-                '<div class="name for-input">%s</div>' .
-                    '<div class="value">' .
-                    '<div class="flexbox middle space-8">' .
-                    '%s' .
-                    '<a class="button light-gray nowrap js-b2b-open-storefront small" href="#" target="_blank" rel="noopener" aria-disabled="true">' .
-                    'Открыть на витрине' .
-                    '</a>' .
-                    '</div>' .
-                    '%s' .
-                    '</div>',
-            ],
-
-            'auth_required' => [
-                'title' => 'Требовать авторизацию',
-                'description' => 'Доступ к B2B-порталу будет разрешён только авторизованным клиентам.',
-                'control_type' => waHtmlControl::CHECKBOX,
-                'class' => 'checkbox',
-                'value' => ifset($values, 'auth_required', 1),
-            ],
-        ];
     }
 
     public function getFormHtml(array $channel): string
@@ -85,19 +16,24 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
         try {
             $view = wa('shop')->getView();
 
-            $template = wa()->getAppPath(
-                'plugins/b2b/templates/actions/B2bSalesChannelForm.html',
-                'shop'
-            );
+            $template = wa()->getAppPath('plugins/b2b/templates/actions/B2bSalesChannelForm.html', 'shop');
 
             if (!file_exists($template)) {
                 throw new waException('Template not found: ' . $template);
             }
 
+            $params = ifset($channel, 'params', []);
+
+            $frontend_from_root = !empty($params['frontend_from_root'])
+                || ifset($params, 'frontend_url', '') === '*';
+
             $view->assign([
                 'channel' => $channel,
                 'base_fields' => $this->getBaseRenderedFields($channel),
-                'b2b_fields' => $this->getB2bRenderedFields($channel),
+                'route_options' => $this->getShopRouteOptions(),
+                'frontend_from_root' => $frontend_from_root,
+                'frontend_custom_url' => $this->getFrontendCustomUrl($params),
+                'auth_required' => !empty($params['auth_required']) || !array_key_exists('auth_required', $params),
             ]);
 
             return $view->fetch('file:' . $template);
@@ -110,6 +46,7 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
             throw $e;
         }
     }
+
     protected function getBaseRenderedFields(array $channel): array
     {
         $result = [];
@@ -127,39 +64,6 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
         return $result;
     }
 
-    protected function getB2bRenderedFields(array $channel): array
-    {
-        $result = [];
-
-        if (isset($channel['params']['frontend_url'])) {
-            $channel['params']['frontend_url'] = trim($channel['params']['frontend_url'], '/*');
-        }
-
-        $field_params = ['namespace' => 'data[params]'] + $this->getFormFieldParams();
-
-        foreach ($this->getFormFieldsConfig(ifset($channel, 'params', [])) as $name => $row) {
-            try {
-                $value = ifset($channel['params'], $name, ifset($row, 'value', ''));
-                $result[$name] = $this->getControl($name, $value, $field_params + $row);
-            } catch (waException $e) {
-                continue;
-            }
-        }
-
-        return $result;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
     // Проверяет и нормализует параметры канала перед сохранением.
     public function sanitizeAndValidateParams(?int $id, array &$params, $params_mode): array
     {
@@ -167,7 +71,6 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
 
         $params['route_key'] = trim((string) ifset($params, 'route_key', ''));
         $params['frontend_from_root'] = !empty($params['frontend_from_root']) ? 1 : 0;
-        $params['frontend_url'] = trim((string) ifset($params, 'frontend_url', ''));
         $params['auth_required'] = !empty($params['auth_required']) ? 1 : 0;
 
         if ($params['route_key'] === '') {
@@ -190,20 +93,25 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
             return $errors;
         }
 
+        $custom_url = $this->normalizeFrontendCustomUrl(ifset($params, 'frontend_custom_url', ''));
+
+        if ($custom_url === '') {
+            $custom_url = $this->normalizeFrontendCustomUrl(ifset($params, 'frontend_url', ''));
+        }
+
+        if ($custom_url === '') {
+            $custom_url = 'b2b';
+        }
+
+        // Последний пользовательский URL храним отдельно, чтобы он не терялся при режиме "от корня".
+        $params['frontend_custom_url'] = $custom_url;
+
         if ($params['frontend_from_root']) {
             // Канал забирает корень выбранного поселения.
+            // frontend_custom_url при этом сохраняется.
             $params['frontend_url'] = '*';
         } else {
-            if ($params['frontend_url'] === '') {
-                $errors[] = [
-                    'field' => 'data[params][frontend_url]',
-                    'error_description' => 'Укажите адрес B2B-портала.',
-                ];
-
-                return $errors;
-            }
-
-            $params['frontend_url'] = $this->normalizeFrontendUrl($params['frontend_url']);
+            $params['frontend_url'] = $this->normalizeFrontendUrl($custom_url);
         }
 
         // Дублируем данные поселения в params канала.
@@ -215,52 +123,55 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
         return $errors;
     }
 
-    // Скрипт только включает/выключает поле. Значение поля не очищается.
-    protected function getFrontendFromRootScript(): string
-    {
-        return '
-            <script>
-                (function () {
-                    var checkbox = document.querySelector(\'input[name="data[params][frontend_from_root]"]\');
-                    var input = document.querySelector(\'input[name="data[params][frontend_url]"]\');
-
-                    if (!checkbox || !input) {
-                        return;
-                    }
-
-                    var toggle = function () {
-                        input.disabled = checkbox.checked;
-                    };
-
-                    checkbox.addEventListener("change", toggle);
-                    toggle();
-                })();
-            </script>
-        ';
-    }
-
-    // Нормализует URL внутри поселения.
+    // Нормализует route pattern внутри поселения для routing.
     protected function normalizeFrontendUrl($url): string
     {
-        $url = trim((string) $url);
-        $url = trim($url, '/');
-
-        if ($url === '' || $url === '*') {
-            return '*';
-        }
-
-        if (substr($url, -1) === '*') {
-            return rtrim($url, '/');
-        }
-
-        $url = preg_replace('/[^a-zа-я0-9\-]/ui', '', $url);
-        $url = mb_strtolower($url, 'UTF-8');
+        $url = $this->normalizeFrontendCustomUrl($url);
 
         if ($url === '') {
             return '*';
         }
 
         return $url . '/*';
+    }
+
+    // Нормализует пользовательский URL для хранения и показа в поле ввода.
+    protected function normalizeFrontendCustomUrl($url): string
+    {
+        $url = trim((string) $url);
+        $url = str_replace('*', '', $url);
+        $url = trim($url, '/');
+
+        if ($url === '') {
+            return '';
+        }
+
+        $url = preg_replace('/[^a-zа-я0-9\-]/ui', '', $url);
+        $url = mb_strtolower($url, 'UTF-8');
+
+        return $url;
+    }
+
+    // Возвращает последний пользовательский URL без routing-mask `/*`.
+    protected function getFrontendCustomUrl(array $params): string
+    {
+        $url = $this->normalizeFrontendCustomUrl(ifset($params, 'frontend_custom_url', ''));
+
+        if ($url !== '') {
+            return $url;
+        }
+
+        $url = ifset($params, 'frontend_url', '');
+
+        if ($url !== '*') {
+            $url = $this->normalizeFrontendCustomUrl($url);
+
+            if ($url !== '') {
+                return $url;
+            }
+        }
+
+        return 'b2b';
     }
 
     // Дополнительные действия после сохранения канала сейчас не требуются.
@@ -273,6 +184,9 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
             [
                 'value' => '',
                 'title' => 'Выберите поселение',
+                'domain' => '',
+                'route_id' => '',
+                'route_url' => '',
             ],
         ];
 
@@ -287,6 +201,9 @@ class shopB2bPluginSalesChannelType extends shopSalesChannelType
                 $options[] = [
                     'value' => $this->buildRouteKey($domain, $route_id),
                     'title' => $this->formatSettlementTitle($domain, $url),
+                    'domain' => $domain,
+                    'route_id' => $route_id,
+                    'route_url' => $url,
                 ];
             }
         }
