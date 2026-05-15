637774e8476d6981fae3ffc073a841bda1c9a129
Vagram <tigra_pv@mail.ru>
2026-05-15 14:24:01 +0300

    b2b

--- wa-apps/shop/plugins/b2b/lib/layouts/shopB2bPluginFrontend.layout.php
+++ wa-apps/shop/plugins/b2b/lib/layouts/shopB2bPluginFrontend.layout.php
@@ -4,12 +4,12 @@ class shopB2bPluginFrontendLayout extends waLayout
 {
     public function execute()
     {
-        $this->view->assign(array(
+        $this->view->assign([
             // 'b2b_route'       => $route,
             // 'b2b_menu'        => $route ? shopLkPluginNavigation::getMenu($route) : array(),
             // 'b2b_cabinet_url' => $route ? shopLkPluginUrlService::getCabinetUrl($route) : wa()->getAppUrl('shop'),
             // 'b2b_user'        => wa()->getUser(),
             'b2b_static_url'  => wa('shop')->getPlugin('b2b')->getPluginStaticUrl(),
-        ));
+        ]);
     }
 }
